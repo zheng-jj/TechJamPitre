aad9d6c7cea1987e6136a80405a24db192cce0ac
Jarren San <sanweirong@hotmail.com>
2025-08-30 23:06:08 +0800

    added id to metadata

--- dev_scripts/model/utils.py
+++ dev_scripts/model/utils.py
@@ -1,5 +1,5 @@
 from langchain_core.documents import Document
+import json
 def jsonl_to_document(jsonl_string):
     json_list = jsonl_string.split("\n")
-    return [Document(json) for json in json_list]
-        
+    return [Document(j, metadata={"id": json.loads(j)["id"]}) for j in json_list if j]
